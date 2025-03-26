55b7f6f9f70c3d72f317161456f83929fbd845ae
johnp-05 <johndavispambi05@gmail.com>
2025-03-26 12:12:08 -0500

    PRUEBA
    COMENTE EL ARCHIVO

--- src/actividad/Modelo.java
+++ src/actividad/Modelo.java
@@ -1,5 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * UNIDAD EDUCATIVA TÉCNICO
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
