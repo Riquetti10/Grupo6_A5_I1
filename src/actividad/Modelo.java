df9a9356903eb23e78b4de89c479782e2e907ef6
Riquetti10 <juan.riquettir.est@uets.edu.ec>
2025-03-26 12:10:29 -0500

    test2

--- src/actividad/Modelo.java
+++ src/actividad/Modelo.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,5 +7,5 @@ package actividad;
 
 public class Modelo 
 {
-    
+     
 }
